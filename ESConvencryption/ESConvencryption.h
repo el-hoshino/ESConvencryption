5a7aea5575a3f968bf2b60cd36f4ffaf8a9e5134
星野恵瑠 <shonetesmith@gmail.com>
2015-09-09 00:27:22 +0900

    Add initial framework project

---
+++ ESConvencryption/ESConvencryption.h
@@ -0,0 +1,19 @@
+//
+//  ESConvencryption.h
+//  ESConvencryption
+//
+//  Created by 史翔新 on 2015/09/09.
+//  Copyright © 2015年 史翔新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESConvencryption.
+FOUNDATION_EXPORT double ESConvencryptionVersionNumber;
+
+//! Project version string for ESConvencryption.
+FOUNDATION_EXPORT const unsigned char ESConvencryptionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESConvencryption/PublicHeader.h>
+
+
